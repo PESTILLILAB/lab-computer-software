b378506e2289c33e0adcb421a573bed5389d781c
Brad Caron <bacaron@utexas.edu>
2023-07-10 17:05:31 -0500

    Reason: small change

--- lab-computer-software-install.sh
+++ lab-computer-software-install.sh
@@ -37,7 +37,6 @@ sudo add-apt-repository -y ppa:apptainer/ppa
 sudo apt update
 sudo apt install -y apptainer-suid
 
-
 # install freesurfer
 curl https://surfer.nmr.mgh.harvard.edu/pub/dist/freesurfer/7.4.1/freesurfer-linux-ubuntu20_amd64-7.4.1.tar.gz --output ~/freesurfer.tar.gz
 tar -zxpf ~/freesurfer.tar.gz
